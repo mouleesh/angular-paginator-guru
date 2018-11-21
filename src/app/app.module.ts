e4ff98954d342ce47641cfb8788a5986e9d31817
Gurunathan Mouleeshwaran (UST <C86078@ust-global.com>
2018-11-21 16:36:54 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {PaginatorModule} from './modules/paginator/paginator.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
